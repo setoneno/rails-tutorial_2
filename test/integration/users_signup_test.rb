6cd0af47a133c8f7a0c8743304b3a6c9d3bb4d73
setoneno <your.email@example.com>
2020-01-14 07:46:44 +0000

    Implement basic login

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -1,7 +1,6 @@
 require 'test_helper'
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
-
   test "invalid signup information" do
     get signup_path
     assert_no_difference 'User.count' do
@@ -11,10 +10,8 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                          password_confirmation: "bar" } }
     end
     assert_template 'users/new'
-    assert_select 'div#error_explanation'
-    assert_select 'div.field_with_errors'
   end
-  
+
   test "valid signup information" do
     get signup_path
     assert_difference 'User.count', 1 do
@@ -23,7 +20,10 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                          password:              "password",
                                          password_confirmation: "password" } }
     end
+    # redirect_to @user
     follow_redirect!
+    # Test
     assert_template 'users/show'
+    assert is_logged_in?
   end
 end
