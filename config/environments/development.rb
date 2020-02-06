f6591d2556a337c34b2509209386048a2dc5ebac
setoneno <your.email@example.com>
2020-02-06 09:45:28 +0000

    Add account activation

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,7 +27,10 @@ Rails.application.configure do
   end
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :test
+  host = '893490bc5ed547d7855999ad56937518.vfs.cloud9.us-east-1.amazonaws.com/'     # クラウドIDE
+  config.action_mailer.default_url_options = { host: host, protocol: 'https' }
 
   config.action_mailer.perform_caching = false
 
